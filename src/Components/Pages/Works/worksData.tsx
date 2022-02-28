e9d7b858df06143e211aad5d8dac6c7884da4c60
huyducvo972001 <70436685+huyducvo972001@users.noreply.github.com>
2022-02-28 16:15:09 +0700

    done

--- src/Components/Pages/Works/worksData.tsx
+++ src/Components/Pages/Works/worksData.tsx
@@ -1,63 +1,63 @@
 export const worksData = [
   {
     id: 0,
-    img: "https://i.ytimg.com/an_webp/mqIAhObGzQk/mqdefault_6s.webp?du=3000&sqp=CLvB5pAG&rs=AOn4CLAfKhH5hm74ddVwuo2uCl1ACkVE3Q",
+    img: "https://i.ytimg.com/vi/mqIAhObGzQk/mqdefault.jpg",
     desc: "MUSIC",
     partners: "Murad - Run (Inspired By Alan Walker) [NCN Release]",
     video: "https://www.youtube.com/embed/mqIAhObGzQk",
   },
   {
     id: 1,
-    img: "https://i.ytimg.com/an_webp/X5mcY8ecs8I/mqdefault_6s.webp?du=3000&sqp=COLE5pAG&rs=AOn4CLBdDRug2Rd9Hk1U_PLw4dABWMNntA",
+    img: "https://i.ytimg.com/vi/X5mcY8ecs8I/maxresdefault.jpg",
     desc: "MUSIC",
     partners: "Ahrix - Nova",
     video: "https://www.youtube.com/embed/X5mcY8ecs8I",
   },
   {
     id: 2,
-    img: "https://i.ytimg.com/an_webp/wJnBTPUQS5A/mqdefault_6s.webp?du=3000&sqp=CKC85pAG&rs=AOn4CLAP9XVRT9SnM9wjBcqGAqq7SIhIPg",
+    img: "https://i.ytimg.com/vi/eIjZU1cUP6Y/maxresdefault.jpg",
     desc: "MUSIC",
     partners: "Alan Walker - The Spectre",
     video: "https://www.youtube.com/embed/wJnBTPUQS5A",
   },
   {
     id: 3,
-    img: "https://i.ytimg.com/an_webp/8h-fqAnIn0A/mqdefault_6s.webp?du=3000&sqp=COCp5pAG&rs=AOn4CLBk8JP8zu3MBTGaQjAqCFe_YAuBrQ",
+    img: "https://i.ytimg.com/vi/8h-fqAnIn0A/maxresdefault.jpg",
     desc: "MUSIC",
     partners: "Ampyx - Rise",
     video: "https://www.youtube.com/embed/8h-fqAnIn0A",
   },
   {
     id: 4,
-    img: "https://i.ytimg.com/an_webp/cTlshvPrIZo/mqdefault_6s.webp?du=3000&sqp=CMTk5pAG&rs=AOn4CLCfrrBh9WfED7mzjXx1e_aWxybsxg",
+    img: "https://i.ytimg.com/vi/cTlshvPrIZo/maxresdefault.jpg",
     desc: "MUSIC",
     partners: "Amadeus - Legendary",
     video: "https://www.youtube.com/embed/cTlshvPrIZo",
   },
   {
     id: 5,
-    img: "https://i.ytimg.com/an_webp/ADBe9c1sUmE/mqdefault_6s.webp?du=3000&sqp=CMTX5pAG&rs=AOn4CLARkG23tt0EQTMUPq3-x4TM7CoWMA",
+    img: "https://i.ytimg.com/vi/ilw-qmqZ5zY/maxresdefault.jpg",
     desc: "MUSIC",
     partners: "Reality - Lost Frequencies",
     video: "https://www.youtube.com/embed/GIDoQsQyS0s",
   },
   {
     id: 6,
-    img: "https://i.ytimg.com/an_webp/mOivOlP9GRk/mqdefault_6s.webp?du=3000&sqp=CJrD5pAG&rs=AOn4CLAMcaiP8uERi2Ql2ZFeZqL9ayMgrw",
+    img: "https://i.ytimg.com/vi/mOivOlP9GRk/maxresdefault.jpg",
     desc: "MUSIC",
     partners: "Alan Walker vs Coldplay - Hymn For The Weekend [Remix]",
     video: "https://www.youtube.com/embed/mOivOlP9GRk",
   },
   {
     id: 7,
-    img: "https://i.ytimg.com/an_webp/13ARO0HDZsQ/mqdefault_6s.webp?du=3000&sqp=CNit5pAG&rs=AOn4CLBCahju8MUp-UwiL6rVKq78nACQSg",
+    img: "https://i.ytimg.com/vi/13ARO0HDZsQ/maxresdefault.jpg",
     desc: "MUSIC",
     partners: "Different Heaven - Safe And Sound [NCS Release]",
     video: "https://www.youtube.com/embed/13ARO0HDZsQ",
   },
   {
     id: 8,
-    img: "https://i.ytimg.com/an_webp/CNsvtIK2Jww/mqdefault_6s.webp?du=3000&sqp=CPTJ5pAG&rs=AOn4CLBxVzeuRprc1ddsB059X4uTCjU5vA",
+    img: "https://i.ytimg.com/vi/CNsvtIK2Jww/maxresdefault.jpg",
     desc: "GAME SHOW",
     partners:
       "7 Nụ Cười Xuân 5 | Tập 11: Trường Giang cạn lời khi Lan Ngọc chúc Tết, Lâm Vỹ Dạ gánh tạ Thúy Ngân",
@@ -65,7 +65,7 @@ export const worksData = [
   },
   {
     id: 9,
-    img: "https://i.ytimg.com/an_webp/_MZk5G9zKik/mqdefault_6s.webp?du=3000&sqp=CPng5pAG&rs=AOn4CLBnRtDakvHUKbszTJ2kw2SpzQHgvg",
+    img: "https://i.ytimg.com/vi/_MZk5G9zKik/maxresdefault.jpg",
     desc: "GAME SHOW",
     partners:
       "Tổng hợp 6 MÀN TRÌNH DIỄN rực cháy của BỘ 6 QUYỀN LỰC cùng TOP 8 CHUNG KẾT RAP VIỆT MÙA 2",
@@ -73,7 +73,7 @@ export const worksData = [
   },
   {
     id: 10,
-    img: "https://i.ytimg.com/an_webp/FeKyLz7q8Xs/mqdefault_6s.webp?du=3000&sqp=CMLl5pAG&rs=AOn4CLCnBHlBr1h0J5UeKqagkQMqHUMz7g",
+    img: "https://i.ytimg.com/vi/FeKyLz7q8Xs/maxresdefault.jpg",
     desc: "GAME SHOW",
     partners:
       "Giọng ải giọng ai 5 | Tập 9 Full: A XÌN đau não khi CỜ CÁ NGỰA cãi nhau tới tấp, bóc mẽ nhau um trời",
@@ -81,7 +81,7 @@ export const worksData = [
   },
   {
     id: 11,
-    img: "https://i.ytimg.com/an_webp/s_3ZgF5IhRg/mqdefault_6s.webp?du=3000&sqp=CJjZ5pAG&rs=AOn4CLB3WZ2h_gKsx2g_U7V8-3iJw-rklg",
+    img: "https://i.ytimg.com/vi/s_3ZgF5IhRg/maxresdefault.jpg",
     desc: "GAME SHOW",
     partners:
       'Thách thức danh hài 4 |tập 9: Cô gái ẵm 100 triệu nhờ khiến Trường Giang, Trấn Thành "ôm bụng"',
@@ -89,7 +89,7 @@ export const worksData = [
   },
   {
     id: 12,
-    img: "https://i.ytimg.com/an_webp/rduDXvi99ig/mqdefault_6s.webp?du=3000&sqp=COjO5pAG&rs=AOn4CLCWeRFfL5Pg5vHvCTBLVAX_KzTJng",
+    img: "https://i.ytimg.com/vi/rduDXvi99ig/maxresdefault.jpg",
     desc: "GAME SHOW",
     partners:
       "Siêu Trí Tuệ Việt Nam mùa 2 - Tập 11: Trận chiến cuối - “song Hoàng” trở lại với màn đấu nghẹt thở",
@@ -97,7 +97,7 @@ export const worksData = [
   },
   {
     id: 13,
-    img: "https://i.ytimg.com/an_webp/3z5RgI_9i8I/mqdefault_6s.webp?du=3000&sqp=COTh5pAG&rs=AOn4CLBE-LBwKFJ-6n9vKxASLVQaCqhK0w",
+    img: "https://i.ytimg.com/vi/3z5RgI_9i8I/mqdefault.jpg",
     desc: "SHORT FILM",
     partners:
       "[Phim ngắn] Người Yêu Nhặt Được - Phim ngắn Valentine cảm động 2022 | TWS Media",
@@ -105,14 +105,14 @@ export const worksData = [
   },
   {
     id: 14,
-    img: "https://i.ytimg.com/an_webp/OzolbHQsQZM/mqdefault_6s.webp?du=3000&sqp=CKzn5pAG&rs=AOn4CLA6BmG8oqifqSc-6SgeW2wiTTx4PQ",
+    img: "https://i.ytimg.com/vi/OzolbHQsQZM/maxresdefault.jpg",
     desc: "SHORT FILM",
     partners: "Phim Ngắn 2022 Hay Nhất | SIÊU LỪA | Lan Hương, Lương Huy",
     video: "https://www.youtube.com/embed/OzolbHQsQZM",
   },
   {
     id: 15,
-    img: "https://i.ytimg.com/an_webp/jf6heRSx4us/mqdefault_6s.webp?du=3000&sqp=CPPO5pAG&rs=AOn4CLCv-65Cb10oA7py3xMp_CSppecV2w",
+    img: "https://i.ytimg.com/vi/jf6heRSx4us/maxresdefault.jpg",
     desc: "SHORT FILM",
     partners:
       "[Phim ngắn] NGÀY EM ĐẾN - CRUSH NĂM 17 TUỔI CỦA TÔI - NYC channel - 24 Entertainment",
@@ -120,7 +120,7 @@ export const worksData = [
   },
   {
     id: 16,
-    img: "https://i.ytimg.com/an_webp/d8JfuIWgtig/mqdefault_6s.webp?du=3000&sqp=CLjs5pAG&rs=AOn4CLDhNbV5GWVeCPtgbuRCNikyhHfMFg",
+    img: "https://i.ytimg.com/vi/d8JfuIWgtig/maxresdefault.jpg",
     desc: "SHORT FILM",
     partners:
       "[Phim ngắn] Người Vợ Không Hoàn Mỹ | Phim Ngắn Cảm Động Về Tình Cảm Vợ Chồng | Joie Baby - TBR Media",
@@ -128,7 +128,7 @@ export const worksData = [
   },
   {
     id: 17,
-    img: "https://i.ytimg.com/an_webp/xKPyXiyAc78/mqdefault_6s.webp?du=3000&sqp=CNrC5pAG&rs=AOn4CLAENZWJGanFpTe2qiG77Izn_oej9Q",
+    img: "https://i.ytimg.com/vi/xKPyXiyAc78/maxresdefault.jpg",
     desc: "SHORT FILM",
     partners:
       "CHỦ TỊCH THỬ LÒNG VỢ SẮP CƯỚI - Phim Ngắn Ý Nghĩa | Quà Tặng Cuộc Sống | Phim Tết 2022 SVM TV",
@@ -136,7 +136,7 @@ export const worksData = [
   },
   {
     id: 18,
-    img: "https://i.ytimg.com/an_webp/rIXhXaQ8tiM/mqdefault_6s.webp?du=3000&sqp=CNTp5pAG&rs=AOn4CLCU78gn7kR5xiMhxB6oErG_tAbdUQ",
+    img: "https://i.ytimg.com/vi/rIXhXaQ8tiM/maxresdefault.jpg",
     desc: "MV",
     partners:
       "NGÀY ĐẦU TIÊN - ĐỨC PHÚC | OFFICIAL MUSIC VIDEO | VALENTINE 2022",
@@ -144,14 +144,14 @@ export const worksData = [
   },
   {
     id: 19,
-    img: "https://i.ytimg.com/an_webp/D80QnP0iRBo/mqdefault_6s.webp?du=3000&sqp=CM7h5pAG&rs=AOn4CLCIPFMHZFjYcKLmKGnZvWKfl_hXHw",
+    img: "https://i.ytimg.com/vi/D80QnP0iRBo/maxresdefault.jpg",
     desc: "MV",
     partners: "ERIK - Đau Nhất Là Lặng Im (Official Music Video)",
     video: "https://www.youtube.com/embed/D80QnP0iRBo",
   },
   {
     id: 20,
-    img: "https://i.ytimg.com/an_webp/Q6ZNsHvspEg/mqdefault_6s.webp?du=3000&sqp=CIDE5pAG&rs=AOn4CLAcnSU8XzzJBZGlAkrHKwU-ny1QGA",
+    img: "https://i.ytimg.com/vi/Q6ZNsHvspEg/maxresdefault.jpg?v=61d06dbe",
     desc: "MV",
     partners:
       "Hoàng Thuỳ Linh & ĐEN - Gieo Quẻ (Casting Coins) | Official Music Video",
@@ -159,14 +159,14 @@ export const worksData = [
   },
   {
     id: 21,
-    img: "https://i.ytimg.com/an_webp/RaoBKCKIDAI/mqdefault_6s.webp?du=3000&sqp=CL7l5pAG&rs=AOn4CLCnTp-rxGD8993EScrd9ey9oaYHqw",
+    img: "https://i.ytimg.com/vi/RaoBKCKIDAI/maxresdefault.jpg",
     desc: "MV",
     partners: "CHẠY VỀ NƠI PHÍA ANH - KHẮC VIỆT x Thảo Bebe | Official MV",
     video: "https://www.youtube.com/embed/RaoBKCKIDAI",
   },
   {
     id: 21,
-    img: "https://i.ytimg.com/an_webp/95ahbau-rJk/mqdefault_6s.webp?du=3000&sqp=COLm5pAG&rs=AOn4CLBZ3ouETx2RsggObbXXIP53IIc-Ng",
+    img: "https://i.ytimg.com/vi/95ahbau-rJk/maxresdefault.jpg",
     desc: "MV",
     partners: "Ex's Hate Me - B Ray x Masew (Ft AMEE) | Official MV",
     video: "https://www.youtube.com/embed/95ahbau-rJk",
